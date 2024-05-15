54c108edf5d9afd3ea5e57aee8152c50ad803780
ricardo santacruz <rjgarzon1@gmail.com>
2024-05-15 14:20:33 -0500

    cambiso

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -9,6 +9,7 @@ namespace PartsUnlimited.Models
     //prueba de versionamiento
 
     //cambio
+    //otra mejorado
     public class CartItem
     {
         [Key]
